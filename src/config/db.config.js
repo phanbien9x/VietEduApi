a131c8cf6002ecd3f47fdf3aeb6646503c8fb092
Phan Bien <phanbien9x@gmail.com>
2022-03-26 08:04:43 +0700

    change config hosting mySql

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  HOST: 'localhost',
-  USER: 'root',
-  PASSWORD: '',
-  DB: 'vieteduapi',
-  PORT: '',
+  HOST: 'sql6.freemysqlhosting.net',
+  USER: 'sql6481533',
+  PASSWORD: 'NuNL8NXBbP',
+  DB: 'sql6481533',
+  PORT: '3306',
   // can't change this const
   PASSWORD_DUMMY_STRING: 'DAUKIET',
 };
