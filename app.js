41f009dfb2c557bad315d121d203753af9f3e5e9
Phan Bien <phanbien9x@gmail.com>
2022-03-26 15:11:15 +0700

    update swagger

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express, { json, urlencoded } from 'express';
+import swaggerUI from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
@@ -8,16 +10,33 @@ import loginRouter from './src/routes/loginRouter';
 config();
 
 const port = process.env.PORT || 8080;
-const app = express();
 
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Vietedu API',
+      version: '1.0.0',
+      description: 'A simple API',
+    },
+    servers: [
+      {
+        url: `https://vieteduapi.herokuapp.com/api`,
+      },
+    ],
+  },
+  apis: ['./src/routes/*.js'],
+};
+
+const specs = swaggerJsDoc(options);
+
+const app = express();
 app.use(helmet());
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
-app.get('/api', (req, res) => {
-  res.json({ message: 'Welcome to vietedu!' });
-});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 app.use('/api/user', userRouter);
 app.use('/api', loginRouter);
 app.listen(port, () => {
