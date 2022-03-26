a02781e3a1b811006ce77a0f8a2a148669a3264d
Phan Bien <phanbien9x@gmail.com>
2022-03-26 11:01:43 +0700

    change db config

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,6 +1,6 @@
-export const HOST = 'localhost';
-export const USER = 'root';
-export const PASSWORD = '';
-export const DB = 'vieteduapi';
-export const PORT = '';
+export const HOST = 'sql6.freemysqlhosting.net';
+export const USER = 'sql6481533';
+export const PASSWORD = 'NuNL8NXBbP';
+export const DB = 'sql6481533';
+export const PORT = '3306';
 export const PASSWORD_DUMMY_STRING = 'DAUKIET';
