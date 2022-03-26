748eaf0f423988149de1fa6c67d578d48b662f27
Phan Bien <phanbien9x@gmail.com>
2022-03-26 08:40:50 +0700

    add Procfile

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: 'sql310.epizy.com',
-  USER: 'epiz_31370139',
-  PASSWORD: 'SVKBSnPJPhEsf',
-  DB: 'epiz_31370139_vieteduapi',
+  HOST: 'sql6.freemysqlhosting.net',
+  USER: 'sql6481533',
+  PASSWORD: 'NuNL8NXBbP',
+  DB: 'sql6481533',
   PORT: '3306',
   // can't change this const
   PASSWORD_DUMMY_STRING: 'DAUKIET',
